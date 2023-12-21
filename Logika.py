fe258abf9468a30bfe12d6e4223b5e138033324a
Matsunaru <error1998pl@gmial.com>
2023-12-21 18:21:28 +0100

    Lets start working

---
+++ Logika.py
@@ -0,0 +1,21 @@
+def Wybor(w1, w2, w3, w4, w5, w6, w7, w8, w9):
+    p  = int(input("gdzie chcesz postawić 'x' ?: \n"))
+    if p == 7:
+        w1 = "X"
+    elif p == 8:
+        w2 = "X"
+    elif p == 9:
+        w3 = "X"
+    elif p == 4:
+        w4 = "X"
+    elif p == 5:
+        w5 = "X"
+    elif p == 6:
+        w6 = "X"
+    elif p == 1:
+        w7 = "X"
+    elif p == 2:
+        w8 = "X"
+    elif p == 3:
+        w9 = "X"
+    return w1, w2, w3, w4, w5, w6, w7, w8, w9
--- Main_Game.py
+++ Main_Game.py
@@ -1,3 +1,29 @@
 import os
+from Logika import *
 
 os.system('cls')
+
+w1 = " "
+w2 = " "
+w3 = " "
+w4 = " "
+w5 = " "
+w6 = " "
+w7 = " "
+w8 = " "
+w9 = " "
+
+i = 0
+while True:
+    if i == 5:
+        break
+    w1, w2, w3, w4, w5, w6, w7, w8, w9 = Wybor(w1, w2, w3, w4, w5, w6, w7, w8, w9)
+
+    os.system('cls')
+    
+    print(w1,"|",w2,"|",w3)
+    print("__________")
+    print(w4,"|",w5,"|",w6)
+    print("__________",)
+    print(w7,"|",w8,"|",w9,)
+    i += 1
